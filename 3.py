bda9d64c1509cb3475eacdfcc214dd4e9a5ebf53
Gilad Ouzan <142928003+giladouzan1@users.noreply.github.com>
2024-02-18 19:01:06 +0200

    init commit

--- 3.py
+++ 3.py
@@ -1,12 +1,13 @@
 a = 50
 b = 10
+c = 5
 my_name = "Gilad"
 
 if a < 10:
     print("You are Gilad")
 elif my_name == "Gilad":
     print("Found your name")
-elif b > 5:
+elif b > 6:
     print("B is good")
 else:
     print("Halbala")
@@ -16,8 +17,4 @@ my_other_name = "or"
 # I can use only IN operator for solving problem without loops
 if my_other_name in my_list:
     print("I found you")
-
-# IS, an operator that checking the same "ID" and return True or False
-tt = 50
-rr = 50
-print(tt is rr)
+print("Aviel")
